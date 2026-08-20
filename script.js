96969bbe5573781297337b897004729853188770
Ivy Zhang <zgivyivy@gmail.com>
2026-08-19 23:25:50 -0700

    add js year infomation

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get current year
+const year = new Date().getFullYear();
+
+document.getElementById("year").textContent = year;
+console.log("Portfolio loaded")
+console.log("Current year:", year);
