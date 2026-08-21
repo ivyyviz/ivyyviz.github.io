6e01347bdf70142692878f4c7e8d20878683e1d1
Ivy Zhang <zgivyivy@gmail.com>
2026-08-20 23:18:02 -0700

    made up scc and js first project content

--- script.js
+++ script.js
@@ -4,3 +4,14 @@ const year = new Date().getFullYear();
 document.getElementById("year").textContent = year;
 console.log("Portfolio loaded")
 console.log("Current year:", year);
+
+//Get project detail
+const projectButtons = document.querySelectorAll('.project-button');
+
+projectButtons.forEach((button) => {
+    button.addEventListener('click', () => {
+        const projectDetails = button.nextElementSibling
+        
+        projectDetails.classList.toggle('show');
+    });
+});
